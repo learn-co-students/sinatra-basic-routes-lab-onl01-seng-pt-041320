ffd771e7f376c60c0d607f3a6c5898bbf1c5c440
MakoaDeVincent <makoadevincent@gmail.com>
2020-07-02 14:55:37 -1000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Makoa."
+    end
+
+    get '/hometown' do
+        "My hometown is Wahiawa."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is The Hurt by Kalapana."
+    end
+
 end

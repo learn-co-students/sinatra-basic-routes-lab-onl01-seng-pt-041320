4339ce2aaf7f982dc1d0aed232fd1d1f15c3c3d0
Paul_Beckwith <303dog@gmail.com>
2020-07-06 16:59:54 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Paul"
+    end
+
+    get '/hometown' do
+        "My hometown is Denver"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Shovels and Dirt"
+    end
 end

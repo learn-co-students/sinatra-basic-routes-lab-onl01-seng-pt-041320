78d7624fbb6f75d929f2bbf14f1c3c28ddfc2b38
OmarMontemayor <omarmontemayor11@yahoo.com>
2020-07-07 11:44:57 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Omar Montemayor."
+    end
+    get '/hometown' do
+        "My hometown is Flower Mound, Tx."
+    end
+    get '/favorite-song' do
+        "My favorite song is Darth Vader's March."
+    end
 end

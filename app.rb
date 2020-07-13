cf54e28ca7f8857ae82aac1505d1240c92f42a3a
chica25 <patricia@patriciaco.com>
2020-07-13 04:42:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/name' do
+         "My name is Patricia" 
+    end
+
+     get '/hometown' do
+       "My hometown is Astoria, NY"
+    end
+
+    get '/favorite-song' do
+     "My favorite song is Crash Into Me by Dave Matthews Band" 
+    end
+
 end

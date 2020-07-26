ef72cbd4edb7d8be9e42f075263f04fad80dfb7a
celestesmthomas <celestesmthomas@gmail.com>
2020-07-26 11:40:48 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        'Hello, World!'
+    end
+
+    get '/name' do
+        "My name is Celeste"
+    end
+
+    get '/hometown' do
+        "My hometown is Long Beach"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Sometimes by H.E.R."
+    end
+
+
 end

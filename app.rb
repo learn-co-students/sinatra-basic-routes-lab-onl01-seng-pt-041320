82bbdc3b17923a34d42e78d3c7277b2d1bfd6470
eulis01 <eulis01@gmail.com>
2020-08-03 00:23:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do 
+    "My name is Eulis"
+  end
+
+  get "/hometown" do 
+    "My hometown is Pennsylvania"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Moonlight"
+  end
 end
